5fdbf4c237877e46c1a9c5606a231e143236dee7
Sudharsanan R <sudharsananr04@gmail.com>
2025-07-15 21:05:26 +0530

    Fix: prevent error when items is null

--- src/App.js
+++ src/App.js
@@ -53,12 +53,12 @@ function App() {
         search = {search}
         setSearch = {setSearch} />
       <Content 
-        items ={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+        items ={(items || []).filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
         handleCheck ={handleCheck}
         handleDelete ={handleDelete}
       />
       <Footer 
-        length ={items.length}
+        length ={(items || []).length}
       />
     </div>
   ) 
