724708b771800b0e73772748012b926612f6cb0b
Sudharsanan R <sudharsananr04@gmail.com>
2025-07-17 11:47:47 +0530

    Fix blank page and filter error

--- src/Content.js
+++ src/Content.js
@@ -5,7 +5,7 @@ const Content = ({items,handleCheck,handleDelete}) => {
   
  return (
     <main>
-      {(items.length)? (
+      {(items && items.length > 0)? (
         <Itemslist
           items ={items}
           handleCheck ={handleCheck}
